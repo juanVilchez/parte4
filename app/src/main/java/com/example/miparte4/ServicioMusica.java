892bfd276b8f1459864b9a74581e37644b54d3ed
aliianza12 <juanv9664@gmail.com>
2020-05-05 13:16:31 -0500

    Notificaciones para la version de android menor a 8.

--- app/src/main/java/com/example/miparte4/ServicioMusica.java
+++ app/src/main/java/com/example/miparte4/ServicioMusica.java
@@ -1,12 +1,27 @@
 package com.example.miparte4;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
 import android.os.IBinder;
+import android.text.Html;
 import android.widget.Toast;
 
+import androidx.core.app.NotificationCompat;
+
 public class ServicioMusica extends Service {
+    private static final int ID_NOTIFICACION_CREAR = 1;
+    public static final String NOTIFICATION_CHANNEL_ID = "1000";
+    public static final String NOTIFICATION_CHANNEL_NAME = "MICANAL";
+
     MediaPlayer reproductor;
     @Override public void onCreate() {
         Toast.makeText(this,"Servicio creado",
@@ -15,15 +30,61 @@ public class ServicioMusica extends Service {
     }
     @Override
     public int onStartCommand(Intent intenc, int flags, int idArranque) {
+        Intent intencionLlamar = new Intent(Intent.ACTION_DIAL,
+                Uri.parse("tel:555123456"));
+        PendingIntent intencionPendienteLlamar =
+                PendingIntent.getActivity(this,0, intencionLlamar,0);
+        NotificationCompat.Builder notific = new NotificationCompat.Builder(this)
+                .setContentTitle("Creando Servicio de Música")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentText("información adicional")
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(),
+                android.R.drawable.ic_media_play))
+                .setWhen(System.currentTimeMillis() + 1000 * 60 * 60)
+                .setContentInfo("más info")
+                .setTicker("Texto en barra de estado")
+                .setDefaults(Notification.DEFAULT_SOUND)
+                .setStyle(new NotificationCompat.InboxStyle()
+                        .addLine("Nueva Conferencia Los neutrinos")
+                        .addLine("Nuevo curso Android Wear")
+                        .setBigContentTitle("2 notificaciones UPT")
+                        .setSummaryText("info@upt.pe"))
+                .setNumber(2)
+                .addAction(android.R.drawable.ic_menu_call, "llamar", intencionPendienteLlamar)
+                .setContentText(Html.fromHtml("<b>Notificación</b> <u>Android<i>UPT</i></u>"))
+                .setDefaults(Notification.DEFAULT_VIBRATE);
+        // Para lanzar una actividad
+        PendingIntent intencionPendiente = PendingIntent.getActivity(
+                this, 0, new Intent(this, Servicio.class), 0);
+        notific.setContentIntent(intencionPendiente);
+        NotificationManager notificationManager = (NotificationManager)
+                getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel =
+                    new NotificationChannel(
+                            NOTIFICATION_CHANNEL_ID,
+                            NOTIFICATION_CHANNEL_NAME,
+                            NotificationManager.IMPORTANCE_LOW);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(R.color.colorAccent);
+            notificationManager.createNotificationChannel(notificationChannel);
+            notific.setChannelId(NOTIFICATION_CHANNEL_ID);
+        }
+        notificationManager.notify(ID_NOTIFICACION_CREAR, notific.build());
         Toast.makeText(this,"Servicio arrancado "+ idArranque,
                 Toast.LENGTH_SHORT).show();
         reproductor.start();
         return START_STICKY;
     }
+
     @Override public void onDestroy() {
         Toast.makeText(this,"Servicio detenido",
                 Toast.LENGTH_SHORT).show();
         reproductor.stop();
+        //Eliminando la notificacion
+        NotificationManager notificationManager = (NotificationManager)
+                getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.cancel(ID_NOTIFICACION_CREAR);
     }
     @Override public IBinder onBind(Intent intencion) {
         return null;
